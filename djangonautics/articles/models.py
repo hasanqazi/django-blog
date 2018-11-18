108e8859c164f788e67024bf65f013025fb4edeb
hasanqazi <hasanahmedhaf@gmail.com>
2018-11-17 21:51:11 -0600

    Added thumbnails

--- djangonautics/articles/models.py
+++ djangonautics/articles/models.py
@@ -6,9 +6,13 @@ class Article(models.Model):
   slug = models.SlugField()
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
+  thumb = models.ImageField(default='default.png', blank=True)
 
   def __str__(self):
     return self.title
 
   def snippet(self):
-    return self.body[:50] + "..."
+    if (len(self.body)<50):
+      return self.body
+    else:
+      return self.body[:50] + "..."
