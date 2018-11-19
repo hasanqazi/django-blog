7da25ca195e42ae61189bd95bd2e3a882e0a8eeb
hasanqazi <hasanahmedhaf@gmail.com>
2018-11-18 20:51:27 -0600

    Added account saving

--- djangonautics/accounts/views.py
+++ djangonautics/accounts/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.template import RequestContext
 
 # Create your views here.
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #login
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form}, RequestContext(request))
